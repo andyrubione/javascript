0eed4d46a6509d271a866ce1ec3ed5587ffede6f
andyrubione <102333590+andyrubione@users.noreply.github.com>
2022-05-12 12:52:08 -0300

    practica de algoritmo condicional

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ if (aceptado == "si") {
         alert("Bienvenido Matias! esto ya es un algoritmo, pero vamos a hacerlo mas interesante, dale a aceptar para continuar")
         const numero = prompt("Para seguir demostrando mis poderes mentales, vamos a adivinar un numero, elegi un numero del 1 al 10")
         if (numero <= 4 || numero >= 6) {
-            alert("No ese no es, tenes que concentrarte!! los numeros funcionan de forma diferente en la conexion mente microprocesador, ahora tendras que volver a empezar por que todavia no aprendimos bucles, para tu proximo intento probemos con el 5 y vas a ver que funciona")
+            alert("No ese no es, tenes que concentrarte!! los numeros funcionan de forma diferente en la conexion mente microprocesador, ahora tendras que volver a empezar por que todavia no aprendimos bucles, para tu proximo intento probemos con el 5 y vas a ver que funciona!!")
         } else if (numero == 5) {
             console.log("Numero elegido por iniciativa propia: " + numero)
             alert("Excelente!! esa es solo una pequeña muestra de todo lo que puedo hacer! el mundo se rendiria a mis pies... si los tuviera" )
@@ -21,39 +21,4 @@ if (aceptado == "si") {
 } else {
     console.log("Que ser tan aburrido...")
     alert("Que pena!! pero apreta F5 que vas a ver que no te vas a arrepentir!!")
-
 }
-
-
-
-/* 
-const Usuario = prompt("Primero que nada, ingresa tu Nombre: ")
-
-if (Usuario == "Andres"){
-    console.log(Usuario)
-    const contraseña = prompt("Ingrese contraseña: ")
-    
-    if(contraseña === "abc123") {
-        alert("Bienvenido " + Usuario)
-    }else {
-        alert("Contraseña incorrecta")
-    }
-    
-}else {
-    alert("Nombre de Usuario incorrecto")
-}
-
-
-const edad = prompt("Ingrese su edad: ")
-
-    if (edad <= 18 || edad >=40){
-        alert("Los domingos te levantas tarde")
-        console.log(edad)
-    } else if (edad==25){
-        alert("Es momento de hacer algo")
-        console.log(edad)
-    } else{
-        alert("Deberias dormir mas")
-        console.log(edad)
-    } 
- */
